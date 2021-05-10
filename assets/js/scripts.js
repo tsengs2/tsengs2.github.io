0db31748554cd90944de26f66ae3bd287e5e2ab7
Stephanie Tseng <stseng2@andrew.cmu.edu>
2021-05-10 00:10:08 -0400

    Styling updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,15 @@
 function Scroll() {
-    var button = document.getElementById("projects");
-    button.scrollIntoView();
+  var button = document.getElementById("projects");
+  button.scrollIntoView();
+}
+
+// Navigation bar referenced https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_responsive_navbar_dropdown for responsiveness
+
+function burgerIcon() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-
-
-  
+}
